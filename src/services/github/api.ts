d8363876f00a073b997092a7226b8e71a4878d20
Yamada Yasuhiro <yasuhiro.yamada@globis.co.jp>
2018-01-11 15:32:45 +0900

    Impl axios error handling now...

--- src/services/github/api.ts
+++ src/services/github/api.ts
@@ -2,11 +2,19 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
+export interface ApiError extends Error {
+  response: any;
+  message: string;
+}
+
 export default class GitHubApi {
   static getOrgMembers = async(orgName: string) => {
     const API_END_POINT = `${BASE_URL}/orgs/${orgName}/members`;
     // TODO:
     // throw errors when API returns errror
-    return await axios(API_END_POINT);
+    const instance = axios.create({
+      timeout: 1,
+    });
+    return await instance.get(API_END_POINT);
   }
 }
