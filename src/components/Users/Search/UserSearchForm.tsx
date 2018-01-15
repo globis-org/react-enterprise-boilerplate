f9d7fde08079876693ed85c7e84840dae26367f0
Yuka O'oka <oukayuka@ayeate.com>
2018-01-15 15:27:42 +0900

    Applying Formik

--- src/components/Users/Search/UserSearchForm.tsx
+++ src/components/Users/Search/UserSearchForm.tsx
@@ -1,15 +1,49 @@
+import { FormikProps, withFormik } from 'formik';
 import * as React from 'react';
+import * as yup from 'yup';
 
-export interface UserSearchFormProps {
-  onSubmit: () => any;
+interface FormValues {
+  login: string;
 }
 
-const UserSearchForm: React.SFC<UserSearchFormProps> = (props) => {
-  return (
-    <div>
-      <p>フォーム</p>
-    </div>
-  );
-};
+const InnerForm: React.SFC<FormikProps<FormValues>> =
+  (props) =>
+(
+  <form onSubmit={props.handleSubmit}>
+    <input
+      id="login"
+      placeholder="ユーザー名"
+      type="text"
+      value={props.values.login}
+    />
+    {props.touched.login && props.errors.login &&
+    <div>{props.errors.login}</div>}
+    <button
+      type="submit"
+      disabled={props.isSubmitting}
+    >
+      送信
+    </button>
+  </form>
+);
+
+const UserSearchForm = withFormik<FormikProps<FormValues>, FormValues>({
+  mapPropsToValues: () => ({ login: '' }),
+  validationSchema: yup.object().shape({
+    login: yup.string()
+      .max(16, '16文字以内で入力してください')
+      .required('ユーザー名を入力してください'),
+    },
+  ),
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(
+      () => {
+        alert(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+      },
+      1000,
+    );
+  },
+})(InnerForm);
 
 export default UserSearchForm;
--- src/components/Users/Search/index.tsx
+++ src/components/Users/Search/index.tsx
@@ -3,15 +3,11 @@ import * as React from 'react';
 import UserList from '../../../components/common/UserList';
 import UserSearchForm from './UserSearchForm';
 
-class UsersSearchComponent extends React.Component {
-  public render() {
-    return (
-      <div>
-        <UserSearchForm onSubmit={() => console.log('call')} />
-        <UserList users={[]} />
-      </div>
-    );
-  }
-}
+const UsersSearchComponent: React.SFC<{}> = () => (
+  <div>
+    <UserSearchForm />
+    <UserList users={[]} />
+  </div>
+);
 
 export default UsersSearchComponent;
