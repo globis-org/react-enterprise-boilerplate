b1f406d5d03fa8294e74a69ea0c1bf7b8acb5dc8
Yuka O'oka <oukayuka@ayeate.com>
2018-01-16 13:21:56 +0900

    Adding mapDispathToProps to UserSearchForm Container

--- src/containers/Users/Search/UserSearchForm.tsx
+++ src/containers/Users/Search/UserSearchForm.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
+import { searchUsers } from '../../../actions/github';
 import UserSearchFormComponent from '../../../components/Users/Search/UserSearchForm';
 
+const mapDispatchToProps = (dispatch: Dispatch<{}>) => ({
+  onSubmit: () => dispatch(searchUsers()),
+});
+
+/*
 const popAlert = () => {
   alert(JSON.stringify('外の関数が呼ばれました', null, 2));
 };
@@ -9,5 +17,8 @@ const popAlert = () => {
 const UserSearchFormContainer: React.SFC<{}> = () => (
    <UserSearchFormComponent onSubmit={popAlert} />
 );
+*/
+
+const UserSearchFormContainer = connect(null, mapDispatchToProps)(UserSearchFormComponent);
 
 export default UserSearchFormContainer;
