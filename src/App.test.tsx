104779ab8e0fae298892111d9bc40587bdd1f897
Yuka O'oka <oukayuka@ayeate.com>
2018-01-16 15:54:58 +0900

    Modify first test to pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div,
+  );
 });
