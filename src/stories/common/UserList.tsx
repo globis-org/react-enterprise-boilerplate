9c129878f2591d56e59b0c2c0f906373db489227
Yamada Yasuhiro <yasuhiro.yamada@globis.co.jp>
2018-01-22 16:57:57 +0900

    Add User List story

---
+++ src/stories/common/UserList.tsx
@@ -0,0 +1,27 @@
+import { withInfo } from '@storybook/addon-info';
+import { array, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { I18nextProvider } from 'react-i18next';
+
+import UserList from 'components/common/UserList';
+import i18n from 'i18n';
+import { User } from 'services/github';
+import * as data from 'services/github/test/fixtures/users.json';
+
+const users: User[] = (data as any);
+
+storiesOf('User List', module)
+  .addDecorator(story => (
+    <I18nextProvider i18n={i18n}>
+      {story() as JSX.Element}
+    </I18nextProvider>
+  ))
+  .addDecorator(withKnobs)
+  .add('With globis published members', withInfo(
+    'User List with globis published members',
+  )(
+    () => (
+      <UserList users={array('Users', users)} />
+    )),
+  );
