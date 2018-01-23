2c439f165733d86db0269615391a0ed1098d1d37
Yasuhiro Yamada <carolina4th@gmail.com>
2018-01-23 22:05:04 +0900

    Update spec files

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,5 @@
 describe('The Home Page', function() {
-  it('successfully loads', function() {
+  it('ホームページが表示される', function() {
     cy.visit('/') // change URL to match your dev URL
     cy.title().should('include', 'React Enterprize Boilerplate')
   })
--- cypress/integration/user_search_page_spec.js
+++ cypress/integration/user_search_page_spec.js
@@ -1,6 +1,21 @@
 describe('The User Search Page', function() {
-  it('successfully hits', function() {
-    cy.visit('/') // change URL to match your dev URL
+  it('文字数制限のエラーが発生する', function() {
+    cy.visit('/')
+    cy.get('a[href="/users/search"]').click()
+    cy.get('#login').type('01234567891234567')
+    cy.get('button[type=submit]').click()
+    cy.contains('16文字以内で入力してください')
+  })
+
+  it('入力必須エラーが発生する', function() {
+    cy.visit('/')
+    cy.get('a[href="/users/search"]').click()
+    cy.get('button[type=submit]').click()
+    cy.contains('入力必須項目です')
+  })
+
+  it('検索結果が表示される', function() {
+    cy.visit('/')
     cy.get('a[href="/users/search"]').click()
     cy.get('#login').type('taro')
     cy.get('button[type=submit]').click()
